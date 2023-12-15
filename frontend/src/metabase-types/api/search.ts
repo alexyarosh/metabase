d82d1f3b526db9a0a18e457c0f8c0d9041c569d8
Ryan Laurie <iethree@gmail.com>
2023-12-15 15:55:01 -0700

    handle snippets and some other stuff

--- frontend/src/metabase-types/api/search.ts
+++ frontend/src/metabase-types/api/search.ts
@@ -84,6 +84,7 @@ export interface SearchResult {
   creator_id: UserId | null;
   creator_common_name: string | null;
   created_at: string | null;
+  can_write: boolean | null;
 }
 
 export interface SearchListQuery {
