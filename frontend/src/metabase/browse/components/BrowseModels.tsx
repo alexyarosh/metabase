42777f846ffe1923f6471bf32d29eed774ed1a89
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-02-07 23:46:49 -0500

    wip: adding filters for verified models

--- frontend/src/metabase/browse/components/BrowseModels.tsx
+++ frontend/src/metabase/browse/components/BrowseModels.tsx
@@ -21,6 +21,7 @@ import { getLocale } from "metabase/setup/selectors";
 import { isInstanceAnalyticsCollection } from "metabase/collections/utils";
 import { updateSetting } from "metabase/admin/settings/settings";
 import { getCollectionName, groupModels } from "../utils";
+import { getFiltersForBrowseModels } from "../selectors";
 import { CenteredEmptyState } from "./BrowseApp.styled";
 import {
   CollectionHeaderContainer,
@@ -32,6 +33,24 @@ import {
 } from "./BrowseModels.styled";
 import { LastEdited } from "./LastEdited";
 
+type Filter = {
+  filterFunc: (model: SearchResult) => boolean;
+  appliedByDefault: boolean;
+};
+
+const possibleFilters: Record<string, Filter> = {
+  onlyShowVerifiedModels: {
+    filterFunc: (model: SearchResult) => model.moderated_status === "verified",
+    appliedByDefault: true,
+  },
+  hideCSVModels: {
+    // TODO: Find a way to filter out csv uploads
+    filterFunc: (_model: SearchResult) => true,
+    appliedByDefault: true,
+  },
+};
+const defaultFilters = { onlyShowVerifiedModels: true, hideCSVUploads: true };
+
 export const BrowseModels = ({
   modelsResult,
 }: {
@@ -41,9 +60,31 @@ export const BrowseModels = ({
   const { data: models = [], error, isLoading } = modelsResult;
   const locale = useSelector(getLocale);
   const localeCode: string | undefined = locale?.code;
-  const modelsFiltered = models.filter(
-    model => !isInstanceAnalyticsCollection(model.collection),
+  const filtersJSON = useSelector(getFiltersForBrowseModels);
+  let filtersFromServer = {};
+  try {
+    filtersFromServer = JSON.parse(filtersJSON || "{}");
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      dispatch(
+        updateSetting({
+          key: "browse-models-filters",
+          value: JSON.stringify(defaultFilters),
+        }),
+      );
+    }
+  }
+  const activeFilters = { ...defaultFilters, ...filtersFromServer };
+  const filterFunctions = Object.entries(activeFilters).map(
+    ([filterName, shouldApply]) =>
+      shouldApply ? possibleFilters[filterName].filterFunc : () => true,
   );
+  filterFunctions.push(
+    (model: SearchResult) => !isInstanceAnalyticsCollection(model.collection),
+  );
+  const modelsFiltered = filterFunctions.reduce((models, filterFunc) => {
+    return models.filter(filterFunc);
+  }, models);
   const groupsOfModels = groupModels(modelsFiltered, localeCode);
 
   useEffect(() => {
--- frontend/src/metabase/browse/selectors.ts
+++ frontend/src/metabase/browse/selectors.ts
@@ -1,9 +1,16 @@
 import { getSetting } from "metabase/selectors/settings";
 import type { State } from "metabase-types/store";
+import type { Settings } from "metabase-types/api";
 import type { BrowseTabId } from "./utils";
 import { isValidBrowseTab } from "./utils";
 
 export const getDefaultBrowseTab = (state: State): BrowseTabId | null => {
   const defaultBrowseTab = getSetting(state, "default-browse-tab");
   return isValidBrowseTab(defaultBrowseTab) ? defaultBrowseTab : null;
+};
+
+export const getFiltersForBrowseModels = (
+  state: State,
+): Settings["filters-for-browse-models"] => {
+  return getSetting(state, "filters-for-browse-models");
 };
